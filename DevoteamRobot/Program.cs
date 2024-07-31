32437be9707ae69a3b4fa613d7f1932829c7b3d5
Mobra <zagni.m@gmail.com>
2024-07-31 15:16:56 +0200

    Add project files.

---
+++ DevoteamRobot/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class RobotController
+{
+
+}
+    public class Robot
+    {
+        private int m_roomWidth = default;
+        private int m_roomDepth = default;
+        private int m_xPos = default;
+        private int m_yPos = default;
+        
+        private char m_direction = default;
+        
+
+        public Robot (int width, int depth, int startX, int startY, char startDirection)
+        {
+            m_roomWidth = width;
+            m_roomDepth = depth;
+            m_xPos = startX;
+            m_yPos = startY;
+            m_direction = startDirection;
+        }
+    }
